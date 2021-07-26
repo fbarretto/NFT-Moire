8ea36081a203ba69421f0a4bec52f8f2818417b3
fbarretto <kikobarretto@gmail.com>
2021-07-25 21:35:46 -0300

    created Planes class

--- js/main.js
+++ js/main.js
@@ -31,4 +31,32 @@ function main(container) {
   circle.setAttributeNS(null, 'fill', 'red');
   container.appendChild(circle);
 
+  container.setAttribute('style','background-color:black');
+
+  var plane = new Plane(container, 128,Math.random()*360);
+  var plane2 = new Plane(container, 128,Math.random()*360);
+  var plane3 = new Plane(container, 128,Math.random()*360);
 };
+
+class Plane {
+  constructor(container, n, color) {
+    this.color = "hsl("+color+",100%, 50%, 80%)";
+    this.offSet = (Math.random() * (0.2) - 0.1);
+    this.speedOffset = (Math.random() * (0.003-0.001) + 0.001);
+    this.rotation = 0;
+
+    for (var i=0; i< n; i++) {
+      var line = document.createElementNS(svgns, 'line');
+      line.setAttributeNS(null, 'x1', (i*(width/n)-width/2)*1.3+4);
+      line.setAttributeNS(null, 'y1', -height);
+      line.setAttributeNS(null, 'x2', (i*(width/n)-width/2)*1.3+4);
+      line.setAttributeNS(null, 'y2', height);
+      line.setAttributeNS(null, 'stroke-width', 4);
+      line.setAttributeNS(null, 'stroke', this.color);
+      line.setAttributeNS(null, 'transform',
+          'translate('+(width/2)+','+(height/2)+') '
+          +'rotate('+(Math.sin((0.001+this.speedOffset)+this.offSet))*360+')');
+      container.appendChild(line);
+    }
+  }
+}
